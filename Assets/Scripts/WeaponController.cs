228597504ac998096f9a485e4b8e554fb8928c77
Goop92 <wanchaloem.prathumkham@edu.futuregames.nu>
2022-12-15 16:58:58 +0100

    Copy scene

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject Crowbar;
+    public bool CanAttack = true;
+    public float AttackCooldown = 1.0f;
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (CanAttack)
+            {
+                CrowbarAttack();
+            }
+        }
+    }
+
+    public void CrowbarAttack()
+    {
+        CanAttack = false;
+        Animator anim = Crowbar.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+
+
+        StartCoroutine(ResetAttackCooldown());
+    }
+
+    IEnumerator ResetAttackCooldown()
+    {
+        yield return new WaitForSeconds(AttackCooldown);
+        CanAttack = true;
+    }
+
+}
+S
